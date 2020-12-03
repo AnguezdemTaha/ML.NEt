056fcbb28b5c0c1f390bc1fad56ab0f61a7c5b8c
tahaang <anguezdemtaha16@gmail.com>
2020-12-03 13:20:49 +0100

    Ajoutez des fichiers projet.

---
+++ test/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using TestML.Model;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+
+                // Add input data
+                Console.WriteLine("----------------Ecrire votre phrase--------------------");
+                //num2 = Convert.ToString(Console.ReadLine());
+
+                var input = new ModelInput();
+                input.SentimentText = Convert.ToString(Console.ReadLine()); ;
+
+                // Load model and predict output of sample data
+                ModelOutput result = ConsumeModel.Predict(input);
+                //Console.WriteLine($"Text: {input.SentimentText}\nIs Toxic: {result.Prediction}");
+                if (result.Prediction == "1")
+                {
+                    Console.WriteLine($"****Is Toxic****");
+                }
+                else
+                {
+                    Console.WriteLine($"*****Is NOT Toxic*****");
+                }
+            }
+        }
+    }
+}
